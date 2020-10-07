a2f0a31f6d801858dc5b7cf924a7f013557b7133
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-10-07 13:10:18 +0700

    Mencoba fungsi query database di dalam model Mahasiswa

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -2,11 +2,18 @@
 
 class Mahasiswa_model
 {
+    private $table = "mahasiswa";
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
     public function getAllMahasiswa()
     {
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
+        $this->db->query('SELECT * FROM ' . $this->table);
 
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $this->db->resultSet();
     }
 }
