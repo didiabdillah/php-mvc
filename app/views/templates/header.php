8fb42e94ad26abfc93e38713d54a6a7f09e29819
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-10-07 10:34:14 +0700

    Menambahkan variabel BASEURL untuk memudahkan penulisan url secara global

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data["judul"]; ?></title>
-    <link rel="stylesheet" href="http://localhost/php-mvc/public/css/bootstrap.css">
+
+    <link rel="stylesheet" href="<?= BASEURL ?>css/bootstrap.css">
 </head>
 
 <body>
